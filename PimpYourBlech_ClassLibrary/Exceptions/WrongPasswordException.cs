8f3a435da1474977880b9d6e6ff191e9e26d80b7
adrian mäscheder <maescheder.adrian@stud.th-rosenheim.de>
2025-11-29 21:20:45 +0100

    Noch mehr GUI

---
+++ PimpYourBlech_ClassLibrary/Exceptions/WrongPasswordException.cs
@@ -0,0 +1,8 @@
+﻿namespace PimpYourBlech_ClassLibrary.Exceptions;
+
+public class WrongPasswordException:Exception
+{
+    public WrongPasswordException(string message) : base(message)
+    {
+    }
+}
