182580acb0e839c33b57b8a1218a12709111fe8b
adrian mäscheder <maescheder.adrian@stud.th-rosenheim.de>
2025-12-11 23:33:08 +0100

    Warenkorb Einkauf implementiert (Rest folgt)

---
+++ PimpYourBlech_ClassLibrary/Services/Carts/CartService.cs
@@ -0,0 +1,31 @@
+﻿using PimpYourBlech_ClassLibrary.Entities;
+using PimpYourBlech_ClassLibrary.Services.Carts;
+using PimpYourBlech_ClassLibrary.ValueObjects;
+
+public class CartService : ICartService
+{
+    public Cart Cart { get; }
+
+    public List<Product> CartProductsList => Cart.Products;
+
+    public CartService()
+    {
+        Cart = new Cart();
+    }
+
+    public Cart GetCart() => Cart;
+
+    public Cart AddProduct(Product product)
+    {
+        if (product != null)
+            Cart.Products.Add(product);
+
+        return Cart;
+    }
+
+    public void RemoveProduct(Product product)
+    {
+        if (product != null)
+            Cart.Products.Remove(product);
+    }
+}
--- PimpYourBlech_ClassLibrary/ValueObjects/Cart.cs
+++ PimpYourBlech_ClassLibrary/ValueObjects/Cart.cs
@@ -1,6 +1,9 @@
+using PimpYourBlech_ClassLibrary.Entities;
+
 namespace PimpYourBlech_ClassLibrary.ValueObjects;
 
 public class Cart
 {
-    
+    public List<Product> Products { get; set; } = new List<Product>();
+    public Customer Customer { get; set; }
 }
