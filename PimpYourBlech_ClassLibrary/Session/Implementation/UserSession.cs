87649a165dba7cc24890749d7b30b6a7d13d933a
adrian mäscheder <maescheder.adrian@stud.th-rosenheim.de>
2025-12-17 16:34:23 +0100

    Checkout implementieren

--- PimpYourBlech_ClassLibrary/Session/Implementation/UserSession.cs
+++ PimpYourBlech_ClassLibrary/Session/Implementation/UserSession.cs
@@ -1,26 +1,30 @@
 using PimpYourBlech_ClassLibrary.Entities;
+using PimpYourBlech_ClassLibrary.Session;
 
 namespace PimpYourBlech_ClassLibrary.Session.Implementation;
 
-public class UserSession:IUserSession
+public class UserSession : IUserSession
 {
-    public Customer CurrentUser { get; set; }
+    public Customer? CurrentUser { get; set; }
+
     public bool IsLoggedIn => CurrentUser != null;
+
     public bool IsAdmin { get; }
 
-    public Configuration CurrentConfiguration { get; set; }
+    public Configuration? CurrentConfiguration { get; set; }
+
+    public DeliveryAddress? DeliveryAddress { get; set; }
+    
+    public PaymentValues? PaymentValues { get; set; }
+
     public void LogOut()
     {
         CurrentUser = null;
+        DeliveryAddress = null;
     }
 
     public void LogIn(Customer customer)
     {
         CurrentUser = customer;
-    }
-
-    public override string ToString()
-    {
-        return CurrentUser.ToString();
     }
 }
