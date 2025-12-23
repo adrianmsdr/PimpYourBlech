944637a1d3f48313076ff2863fe57b856bc52e4d
adrian mäscheder <maescheder.adrian@stud.th-rosenheim.de>
2025-12-24 00:40:10 +0100

    Startmenü

---
+++ PimpYourBlech_ClassLibrary/Migrations/20251222225359_pimpyourblech.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PimpYourBlech_ClassLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class pimpyourblech : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
