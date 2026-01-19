bde856fa120f26efb5455ef1b42333ffcefda751
Frick Michael <frick.michael@stud.th-rosenheim.de>
2026-01-19 19:55:12 +0100

    Kommentare zu faq und comparator hinzugefügt

--- PimpYourBlech_Contracts/EntityDTOs/CommunityAnswerDto.cs
+++ PimpYourBlech_Contracts/EntityDTOs/CommunityAnswerDto.cs
@@ -2,13 +2,17 @@ namespace PimpYourBlech_Contracts.EntityDTOs;
 
 public class CommunityAnswerDto
 {
+    
+    // Eindeutige ID der Antwort (aus der Datenbank)
     public int Id { get; set; }
 
+    // Gespeicherter Inhalt der Antwort
     public string Content { get; set; } = string.Empty;
 
+    // Zeitpunkt der Erstellung, wird serverseitig gesetzt
     public DateTime CreatedAt { get; set; }
 
-    // Foreign Key
+    // Foreign Key für die zuordnung zur Frage
     public int QuestionId { get; set; }
 
     
--- PimpYourBlech_Contracts/EntityDTOs/CommunityQuestionDto.cs
+++ PimpYourBlech_Contracts/EntityDTOs/CommunityQuestionDto.cs
@@ -3,12 +3,17 @@ namespace PimpYourBlech_Contracts.EntityDTOs;
 public class CommunityQuestionDto
 {
      
+    // Eindeutige ID der Frage
     public int Id { get; set; }
-
+    
+    // Inhalt der Frage
     public string Content { get; set; } = string.Empty;
+    
 
+    // Zeitpunkt der Erstellung der Frage
     public DateTime CreatedAt { get; set; }
 
+    // Alle zugehörigen Antworten zur Frage
     public List<CommunityAnswerDto> Answers { get; set; } = new();
 
     }
